0be2e2770c208904d1f0ae34cb4f8ac105ed8986
newlyfei <newlyfei@163.com>
2018-02-08 18:33:41 +0800

    Initial commit

---
+++ app/src/main/java/com/androiddemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.androiddemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.CellButton);
+        button.setOnClickListener(new MyButtonListener());
+    }
+
+    //监听器类
+    class MyButtonListener implements View.OnClickListener {
+        public void onClick(View v){
+            // 生成一个Intent对象
+            Intent intent=new Intent();
+            intent.putExtra("testIntent", "123");
+            intent.setClass(MainActivity.this, Cell.class); //设置跳转的Activity
+            MainActivity.this.startActivity(intent);
+        }
+    }
+}
