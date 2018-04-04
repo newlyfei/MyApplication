9a2574471826bf1040d910d3af57c99500f8e231
newlyfei <newlyfei@163.com>
2018-04-04 17:06:02 +0800

    百度地图

--- app/src/main/java/cn/hycun/androiddemo/ListStaticActivity.java
+++ app/src/main/java/cn/hycun/androiddemo/ListStaticActivity.java
@@ -1,15 +1,13 @@
-package com.androiddemo;
+package cn.hycun.androiddemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class ListStaticActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_static_list);
+        setContentView(cn.hycun.androiddemo.R.layout.activity_static_list);
     }
 }
--- app/src/main/java/cn/hycun/androiddemo/MainActivity.java
+++ app/src/main/java/cn/hycun/androiddemo/MainActivity.java
@@ -1,18 +1,16 @@
-package com.androiddemo;
+package cn.hycun.androiddemo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(cn.hycun.androiddemo.R.layout.activity_main);
 
 //        Button button=(Button)findViewById(R.id.CellButton);
 //        button.setOnClickListener(new MyButtonListener());
